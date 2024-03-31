0fb483f6484ef175d8ef5979e9647722896ff542
sakshi-dhingra <sakshi2210dhingra@gmail.com>
2024-04-01 00:16:34 +0100

    added ssl for mqtt and parsing device name and version

--- data_pipeline/wow_sub.py
+++ data_pipeline/wow_sub.py
@@ -2,6 +2,7 @@ import os
 import paho.mqtt.client as mqtt
 import pandas as pd
 import json
+import ssl
 from datetime import datetime
 
 from commons import *
@@ -11,7 +12,7 @@ DF = None
 def on_mqtt_connect(client, userdata, flags, rc, properties):
     print("Connected with MQTT broker with status", str(rc))
 
-    client.subscribe("test")
+    client.subscribe("/device/data")
 
 
 def on_mqtt_message(client, userdata, msg):
@@ -19,9 +20,10 @@ def on_mqtt_message(client, userdata, msg):
 
     sensor_readings = json.loads(msg.payload.decode())
     data = sensor_readings.get("data")
-
+    device_id = sensor_readings.get("device")
+    version = sensor_readings.get("version")
     timestamp = datetime.now().isoformat()
-    new_row = {'timestamp': timestamp, **data}
+    new_row = {"device": device_id, 'timestamp': timestamp, "version": version, **data}
     DF = DF._append(new_row, ignore_index=True)
     DF.to_parquet(SENSOR_FILE, index=False)
 
@@ -33,10 +35,17 @@ def main():
         DF = pd.read_parquet(SENSOR_FILE)
         print("Loaded existing sensor data file.")
     else:
-        DF = pd.DataFrame(columns=["timestamp", "speed", "length"])
+        DF = pd.DataFrame()
         print("Initialized new DataFrame.")
 
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    # Set up SSL/TLS connection
+    ssl_context = ssl.create_default_context()
+    ssl_context.load_verify_locations("cert.pem")
+
+    client.tls_set_context(ssl_context)
+
     client.on_connect = on_mqtt_connect
     client.on_message = on_mqtt_message
 
@@ -44,6 +53,7 @@ def main():
 
     client.connect(SERVER_HOST, SERVER_PORT, 60)
     client.loop_forever()
+
 
 
 if __name__ == "__main__":
